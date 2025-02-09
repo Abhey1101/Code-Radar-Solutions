62af4a0060519cbdebd2d3deb0cf16a0af64300b
Abhey1101 <abhey161@gmail.com>
2025-02-09 19:30:16 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    char a;
+    scanf("%c", &a);
+    if(a==){
+        printf("Divisible");
+    }
+    else{
+        printf("Not Divisible");
+    }
+    return 0;
+}
